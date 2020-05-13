ae6fc7d24ce03d2d8837603603c6af0f64ce929c
benoit barges <benoitbarges@gmail.com>
2020-05-13 17:04:41 +0200

    adding some bootstraps again

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,25 @@
 class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.create(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :description, :address, :number_of_guests, :price_per_night)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :flats
+  resources :flats, only: [:index, :show, :new, :create]
 end
